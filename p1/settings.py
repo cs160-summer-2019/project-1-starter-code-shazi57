974ded43541d45b4620e33156e6dfaeaee496d94
tary82504 <tary82504@berkeley.edu>
2019-07-03 21:25:25 -0400

    1234

--- p1/settings.py
+++ p1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'dh)m$rf)%qu#%pvyo=7b8gipl6p$i%v6l=_-mxoja$ylc!4rny'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["project-1-starter-code-shazi57-tary82504149924.codeanyapp.com"]
 
 
 # Application definition
